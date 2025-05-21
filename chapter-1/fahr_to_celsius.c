1ad9e4a4a841ef1e6cd5e203eb519fa5ba2d2a7b
idbeholdv18 <alimsadullaev18@gmail.com>
2025-05-21 21:10:54 +0300

    switch while loop to for loop

--- chapter-1/fahr_to_celsius.c
+++ chapter-1/fahr_to_celsius.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    float fahr, celsius;
-    int lower, upper, step;
-
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
     printf("fahrenheit\tcelsius\n");
-    while(fahr <= upper) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%10.0f\t%7.1f\n", fahr, celsius);
-        fahr = fahr + step;
+    for (int fahr = 0; fahr <= 300; fahr += 20) {
+        printf("%10d\t%7.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
     }
 }
