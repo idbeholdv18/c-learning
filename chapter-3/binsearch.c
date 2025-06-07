210a1ffd3614c77f727228a7b9f9c7a65f5963b4
idbeholdv18 <alimsadullaev18@gmail.com>
2025-06-07 17:25:05 +0300

    binsearch function without else_if construction

--- chapter-3/binsearch.c
+++ chapter-3/binsearch.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int binsearch(int x, int v[], int n);
+int binsearch_v2(int x, int v[], int n);
 
 int main() {
     int values[12] = {1, 2, 2, 4, 5, 6, 7, 7, 8, 10, 12, 22};
     int to_find = 10;
 
     printf("%d has index: %d\n", to_find, binsearch(to_find, values, 12));
-
+    printf("%d has index: %d\n", to_find, binsearch_v2(to_find, values, 12));
 }
 
 int binsearch(int x, int v[], int n) {
@@ -26,4 +27,24 @@ int binsearch(int x, int v[], int n) {
         }
     }
     return -1;
+}
+
+int binsearch_v2(int x, int v[], int n) {
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while (low < high) {
+        mid = (low + high) / 2;
+        if (x <= v[mid]) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    if (v[low] == x)
+        return low;
+    else
+        return -1;
 }
