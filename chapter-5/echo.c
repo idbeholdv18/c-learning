f5e1b046df32fb0be8d55fb5b464782859b9d079
idbeholdv18 <alimsadullaev18@gmail.com>
2025-06-11 12:48:30 +0300

    echo program clone

---
+++ chapter-5/echo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    int i = 1;
+    while (i < argc) {
+        printf("%s%s", argv[i], (i < argc - 1) ? " " : "\n");
+        i++;
+    }
+    return 0;
+}
